ce6520c539b4f1dfa3617147c88b4f83174f809c
Nandu A <84287811+nandu-555@users.noreply.github.com>
2024-10-09 01:24:35 +0530

    Created 404(Not Found) Page

--- src/pages/Definition.js
+++ src/pages/Definition.js
@@ -1,34 +1,73 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import { v4 as uuidv4 } from 'uuid';
+import NotFound from "../components/NotFound";
+
 
 export default function Definition() {
     const [word, setWord] = useState();
+    const [notFound, setNotFound] = useState(false);
     let { search } = useParams();
+    const navigate = useNavigate();
+
 
     useEffect(() => {
         fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + search)
-            .then((response) => response.json())
+            .then((response) => {
+                if (response.status === 404) {
+                    setNotFound(true);
+                }
+               return response.json();
+            })
             .then((data) => {
-                setWord(data[0].meanings);
+                if (data && data[0] && data[0].meanings) {
+                    setWord(data[0].meanings);
+                } else {
+                    setNotFound(true);
+                }
                 
-            }
-        );
+            });
     }, []);
+
+    if (notFound === true) {
+        return (
+            <>
+                <NotFound />
+                <Link to="/dictionary">Search another</Link>
+            </>
+        );
+    }
 
     return  (
         <>
-            <h1>Here is a definition:</h1>
-            {word
-                ? word.map((meaning) => {
+            {word ? (
+                <>
+                     <h1>Here is a definition:</h1>
+                {word.map((meaning) => {
                     return (
                         <p key={uuidv4()}> 
                             {meaning.partOfSpeech + ': '}
                             {meaning.definitions[0].definition}
                         </p>
                     );
-                })
-            : null}
+                })}</>
+              )  : null}
         </>
     );
 }
+
+
+
+// useEffect(() => {
+//     fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + search)
+//         .then((response) => {
+//             if (response.status === 404) {
+//                 setNotFound(true);
+//             }
+//            return response.json();
+//         })
+//         .then((data) => {
+//             setWord(data[0].meanings);
+            
+//         });
+// }, []);
