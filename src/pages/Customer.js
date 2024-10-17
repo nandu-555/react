1495b06e17c5998b39af778b589072494360e2a6
Nandu A <84287811+nandu-555@users.noreply.github.com>
2024-10-18 02:48:52 +0530

    create details page and return 404 from Backend API(Django)

---
+++ src/pages/Customer.js
@@ -0,0 +1,44 @@
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import NotFound from "../components/NotFound";
+import { baseUrl } from "../shared";
+
+export default function Customer() {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [customer, setCustomer] = useState();
+    const [notFound, setNotFound] = useState();
+    useEffect(() => {
+        console.log('useEffect');
+        const url = baseUrl + 'api/customers/' + id;
+        fetch(url)
+        .then((response) => {
+            if(response.status === 404){
+                //render a 404 component in this page
+                setNotFound(true);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            setCustomer(data.customer)
+        });
+    }, []);
+    return  (
+    <> 
+        {notFound ? <p>The customer with id {id} was not found</p> : null}
+
+        {customer ? (
+            <div>
+                <p>{customer.id}</p>
+                <p>{customer.name}</p>
+                <p>{customer.industry}</p>
+            </div>
+        ) : null} 
+        <Link to='/customers'>Go back</Link>
+     </>
+    );
+}
+
+
+
+
