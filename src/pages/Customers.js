0fc35ae736d2c54c5bea291ef47c2f143a051fd4
Nandu A <84287811+nandu-555@users.noreply.github.com>
2024-10-24 00:41:58 +0530

    create popup modal to add data(POST)

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,6 +1,7 @@
 import {useEffect, useState} from 'react'
 import { Link } from 'react-router-dom';
 import { baseUrl } from '../shared';
+import AddCustomer from '../components/AddCustomer';
 
 export default function Customers(){
     const [customers, setCustomers] = useState();
@@ -13,9 +14,32 @@ export default function Customers(){
         });
         
     }, []);
+
+    function newCustomer(name, industry) {
+        const data = {name: name, industry: industry};
+        const url = baseUrl + 'api/customers/';
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then((response) => {
+            if(!response.ok){
+                throw new Error('Something went wrong');
+            }
+            return response.json();
+        }).then((data) => {
+            //assume the add was succesful
+            //hide the modal
+            //make sure the list is updated appropriately
+        }).catch((e) => {
+            console.log(e);
+        });
+    }
     return (
     <>
-        <h1>Here are our customer:</h1>
+        <h1>Here are our customers:</h1>
         <ul>
             {customers ? customers.map((customer) => {
                 return (
@@ -25,6 +49,7 @@ export default function Customers(){
                 );
             }) : null}
         </ul>
+        <AddCustomer newCustomer={newCustomer}/>
     </>
     );
 
